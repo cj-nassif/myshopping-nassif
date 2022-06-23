df29e1ae0293b4998a112f6d7356b6ebf76816e2
cj-nassif <nassif.cj@gmail.com>
2022-06-23 17:07:21 -0300

    Configured Firebase Android

--- android/app/src/main/java/com/myshoppingnassif/MainActivity.java
+++ android/app/src/main/java/com/myshoppingnassif/MainActivity.java
@@ -1,4 +1,4 @@
-package com.myshopping;
+package com.myshoppingnassif;
 
 import android.os.Bundle;
 
--- android/app/src/main/java/com/myshoppingnassif/MainApplication.java
+++ android/app/src/main/java/com/myshoppingnassif/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myshopping;
+package com.myshoppingnassif;
 
 import android.app.Application;
 import android.content.Context;
